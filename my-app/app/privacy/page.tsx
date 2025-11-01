fe106f290539c21f7da22950f4d26c37bb10b867
Farzi36 <faris361707@gmail.com>
2025-11-01 21:28:57 +0700

    lupa delete

--- my-app/app/privacy/page.tsx
+++ my-app/app/privacy/page.tsx
@@ -7,12 +7,6 @@ export default function PrivacyPage() {
   return (
     <main className="min-h-screen bg-background flex flex-col">
       <Navigation />
-      <Image
-        src="https://tamanmini.com/taman_jelajah_indonesia/wp-content/uploads/2023/09/Anjungan-Bali-scaled.jpg"
-        alt="Anjungan Bali"
-        width={640}
-        height={480}
-      />
       <UnderConstruction title="Privacy Policy" />
       <Footer />
     </main>
